75e05884229e0069271550d46781d0b5617d93e3
Shusen <shusenl@sci.utah.edu>
2015-09-30 15:03:12 -0600

    add church numerical

---
+++ church_numerical.js
@@ -0,0 +1,7 @@
+//use arrow function definition in ES6
+var zero = (x) => x;
+var number = (n) => { (f) => {return function(z) { for(var i=0; i<n; i++) z = f(z); return z;}  }  };
+var TRUE = (a b) => a;
+var FALSE  = (a b) => b;
+
+
