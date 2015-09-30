afbab91c70fc93fa6dfbe66a444afc90917f983b
Shusen Liu <shusen.liu.hust@gmail.com>
2015-09-30 15:07:16 -0600

    Update church_numerical.js

--- church_numerical.js
+++ church_numerical.js
@@ -1,6 +1,9 @@
 //use arrow function definition in ES6
 var zero = (x) => x;
+
+// short cut, used for which is outside the minimal subset
 var number = (n) => { (f) => {return function(z) { for(var i=0; i<n; i++) z = f(z); return z;}  }  };
+
 var TRUE = (a b) => a;
 var FALSE  = (a b) => b;
 
