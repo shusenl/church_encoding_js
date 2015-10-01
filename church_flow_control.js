9009637fd5835f505df92ca0b88e8db710b0df64
Shusen <shusenl@sci.utah.edu>
2015-09-30 18:11:53 -0600

    add church flow control

---
+++ church_flow_control.js
@@ -0,0 +1,8 @@
+//strict subset of javascript include lambda, single parameter function, function call
+//true false
+var TRUE = a => b => a;
+var FALSE = a => b => b;
+//this following utilized multi-parameter function, therefore, not belong to the strict subset
+//var TRUE = (a b) => a;
+//var FALSE = (a b) => b;
+
--- church_numerical.js
+++ church_numerical.js
@@ -1,10 +1,29 @@
+//strict subset of javascript include lambda, single parameter function, function call
 //use arrow function definition in ES6
-var zero = (x) => x;
+//the following is the  same as: void = function(x){return x;} 
+var void = x => x;
 
-// short cut, used for which is outside the minimal subset
-var number = (n) => { (f) => {return function(z) { for(var i=0; i<n; i++) z = f(z); return z;}  }  };
+var zero = f => z => z; //z indicate zero
+var one = f => z => f(z);
+var two = f => z => f( f(z) );
+var inc = n => f => z => n(f) (f (z) ); //n indicate the input function
 
-var TRUE = (a b) => a;
-var FALSE  = (a b) => b;
+//////// helper function - doesn not restrict to the subset ///////////
+var numberValue = n => n( x => x+1 )(0);
+
+// short cut to generate n, used 'for which is outside the minimal subset
+var number = n => f => {
+    return function(z) { 
+      for(var i=0; i<n; i++) 
+        z = f(z); 
+      return z;}  
+    };
+
+console.log( 'value of zero: ' + numberValue( zero ));
+console.log( 'value of one: ' + numberValue( inc(zero) ));
+console.log( 'value of 10: ' + numberValue( number(10) ));
+
+
+
 
 
